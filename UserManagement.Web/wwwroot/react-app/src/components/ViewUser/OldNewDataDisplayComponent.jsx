ca4fb2dadef1d1491bcbadc51cb95fcf7b3995d1
Siddharth Bahl <bahlsiddharth123@gmail.com>
2025-08-20 16:05:36 +0100

    working on some edge cases

--- UserManagement.Web/wwwroot/react-app/src/components/ViewUser/OldNewDataDisplayComponent.jsx
+++ UserManagement.Web/wwwroot/react-app/src/components/ViewUser/OldNewDataDisplayComponent.jsx
@@ -2,9 +2,9 @@ export default function OldNewDataDisplayComponent({oldData, newData}) {
     return (
         <>
             <span>Old Data</span>
-            <span>{oldData}</span>
+            <span>{oldData !== null ? oldData : "No data to show"}</span>
             <span>New Data</span>
-            <span>{newData}</span>
+            <span>{newData !== null ? newData : "No data to show"}</span>
         </>
     )
 }
